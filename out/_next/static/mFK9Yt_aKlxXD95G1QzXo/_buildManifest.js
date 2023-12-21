db083bc10840735ac61877134ca000534790d436
hugues898 <hugues.christophe@epfl.ch>
2023-12-21 19:22:54 +0100

    ddzd

---
+++ out/_next/static/mFK9Yt_aKlxXD95G1QzXo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/581-bcdf15fb99abe711.js","static/chunks/pages/index-43f860606bdf282d.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
