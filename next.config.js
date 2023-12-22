41be5feb3a4a8fe5c67a8fbd2c18401b7254430f
hugues898 <hugues.christophe@epfl.ch>
2023-12-22 16:24:21 +0100

    dd

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Ajoutez vos autres configurations ici si nécessaire
+    output: 'standalone',
+    basePath: '/gh-pages',
+    assetPrefix: '/gh-pages/',
+  };
+  
+  module.exports = nextConfig;
+  
